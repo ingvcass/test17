2ce60e93560a396d37d58413e97de7d98fb5ce2c
ingvcass <113540264+ingvcass@users.noreply.github.com>
2024-05-08 12:41:50 +0300

    Add files via upload

---
+++ UnitTestProject/FileStorageTest.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnitTestEx;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class FileStorageTest
+    {
+        public const string WRITE_EXCEPTION = "WRITING IS INCORRECT";
+        public const string DELETE_EXCEPTION = "DELETING IS INCORRECT";
+        public const string MAX_SIZE_EXCEPTION = "FILE IS BIGGER THEN MAX SIZE";
+        public const string NULL_FILE_EXCEPTION = "NULL FILE WAS WRITTEN";
+
+        public const string NULL_STRING = "";
+        public const string FILE_PATH_STRING = "@D:\\JDK-intellij-downloader-info.txt";
+        public const string FILE_PATH_STRING2 = "@D:\\JDK-intellij-downloader.txt";
+        public const string CONTENT_STRING = "Some text";
+        public const string WRONG_SIZE_CONTENT_STRING = "TEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtext";
+        public List<File> files = new List<File>();
+
+        public const int NEW_SIZE = 5;
+
+        [TestMethod]
+        public void WriteTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+
+            String file = newFile.GetFilename();
+            storage.Write(newFile);
+            Assert.AreEqual(storage.IsExists(file), true, WRITE_EXCEPTION);
+        }
+        [TestMethod]
+        public void WriteSameTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+
+            storage.Write(newFile);
+            try
+            {
+                storage.Write(newFile);
+            }
+            catch (FileNameAlreadyExistsException) { return; }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+
+            String file = newFile.GetFilename();
+            storage.Write(newFile);
+            storage.Delete(file);
+            Assert.AreEqual(storage.IsExists(file), false, DELETE_EXCEPTION);
+        }
+
+        [TestMethod]
+        public void DeleteAllTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+            storage.Write(newFile);
+            String file = newFile.GetFilename();
+            File newFile2 = new File(FILE_PATH_STRING2, CONTENT_STRING);
+            storage.Write(newFile2);
+            String file2 = newFile.GetFilename();
+            storage.DeleteAllFiles();
+            Assert.AreEqual(storage.IsExists(file2) && storage.IsExists(file), false, DELETE_EXCEPTION);
+        }
+
+        [TestMethod]
+        public void WriteWrongSizeFileTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, WRONG_SIZE_CONTENT_STRING);
+
+            String file = newFile.GetFilename();
+            storage.Write(newFile);
+            Assert.AreEqual(storage.IsExists(file), false, MAX_SIZE_EXCEPTION);
+        }
+
+        [TestMethod]
+        public void GetFilesTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+            storage.Write(newFile);
+            files.Add(newFile);
+            File newFile2 = new File(FILE_PATH_STRING2, CONTENT_STRING);
+            files.Add(newFile2);
+            storage.Write(newFile2);
+            Assert.AreEqual(storage.GetFiles(), files, DELETE_EXCEPTION);
+        }
+
+        [TestMethod]
+        public void GetFileTest() 
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+            String file = newFile.GetFilename();
+            storage.Write(newFile);
+            Assert.AreEqual(storage.GetFile(file), newFile, WRITE_EXCEPTION);
+        }
+
+        [TestMethod]
+        public void WriteEmptyFileTest()
+        {
+            FileStorage storage = new FileStorage(NEW_SIZE);
+            try
+            {
+                File newFile = new File(null, null);
+                storage.Write(newFile);
+            }
+            catch (NullReferenceException) { return; }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+    }
+}
---
+++ UnitTestProject/FileTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using UnitTestEx;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class FileTest
+    {
+
+        public const string SIZE_EXCEPTION = "Wrong size";
+        public const string NAME_EXCEPTION = "Wrong name";
+        public const string EXTENSION_EXCEPTION = "Wrong extension";
+        public const string SPACE_STRING = " ";
+        public const string FILE_PATH_STRING = "@D:\\JDK-intellij-downloader-info.txt";
+        public const string CONTENT_STRING = "Some text";
+        public double lenght;
+
+
+        [TestMethod]
+        public void GetSizeTest()
+        {
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+            String content = CONTENT_STRING;
+
+            lenght = content.Length / 2;
+            Assert.AreEqual(newFile.GetSize(), lenght, SIZE_EXCEPTION);
+        }
+        [TestMethod]
+        public void GetFilenameTest()
+        {
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+            String name = "JDK-intellij-downloader-info";
+            Assert.AreEqual(newFile.GetFilename(), name, NAME_EXCEPTION);
+        }
+        [TestMethod]
+        public void GetExtensionTest()
+        {
+            File newFile = new File(FILE_PATH_STRING, CONTENT_STRING);
+            String extension = "txt";
+            Assert.AreEqual(newFile.GetExtension(), extension, EXTENSION_EXCEPTION);
+        }
+    }
+}
